47cfb807a9d5dc3ad19d4da4e340812cf76917b0
Ali Sanaknaki <asanaknaki@shoplogix.com>
2020-03-14 15:32:18 -0400

    Making moves.

---
+++ build/precache-manifest.1c20744e9a613933307005177c84d0a4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd9c23f9ef5fd8b8cce9b9e968c9c570",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a423e8c4257fde6cc98",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "8c0ae794360c212c6b5d",
+    "url": "/static/css/main.633b5331.chunk.css"
+  },
+  {
+    "revision": "5a423e8c4257fde6cc98",
+    "url": "/static/js/2.f06c0ced.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/static/js/2.f06c0ced.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c0ae794360c212c6b5d",
+    "url": "/static/js/main.bd2c73aa.chunk.js"
+  },
+  {
+    "revision": "63e15175b4308472b624",
+    "url": "/static/js/runtime-main.56ecaa98.js"
+  },
+  {
+    "revision": "fd9d297262c4cc1a70b135c1687f8c22",
+    "url": "/static/media/logo-google.fd9d2972.png"
+  },
+  {
+    "revision": "a0dac9b0ce6d8208b423b0130fa16161",
+    "url": "/static/media/resume_ali.a0dac9b0.pdf"
+  }
+]);
