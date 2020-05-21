756afa11df7e22e9771e50ef36b47b8d06f96bf0
Ali Sanaknaki <asanaknaki@shoplogix.com>
2020-05-21 13:32:03 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1c20744e9a613933307005177c84d0a4.js"
+  "/precache-manifest.4663b615433b1563ac973e633b48e30a.js"
 );
 
 self.addEventListener('message', (event) => {
