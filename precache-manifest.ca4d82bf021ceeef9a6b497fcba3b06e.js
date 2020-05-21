0cef89afea1fbe509da7129540e6b962e3cda685
Ali Sanaknaki <asanaknaki@shoplogix.com>
2020-05-21 12:19:02 -0400

    Updates

---
+++ precache-manifest.ca4d82bf021ceeef9a6b497fcba3b06e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "462891dea849342373d4af60adb656a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "544326c94cc384a5f7ee",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "d9fadcdcf8a3b5cdb1ef",
+    "url": "/static/css/main.98abc787.chunk.css"
+  },
+  {
+    "revision": "544326c94cc384a5f7ee",
+    "url": "/static/js/2.b72ba680.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/static/js/2.b72ba680.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9fadcdcf8a3b5cdb1ef",
+    "url": "/static/js/main.6472c88f.chunk.js"
+  },
+  {
+    "revision": "63e15175b4308472b624",
+    "url": "/static/js/runtime-main.56ecaa98.js"
+  }
+]);
