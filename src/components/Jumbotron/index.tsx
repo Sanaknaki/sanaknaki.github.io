b7809781d73fe8d44e3ab390ac85ac0f5730904e
Ali Sanaknaki <sanaknaki.ali@gmail.com>
2024-07-20 17:41:39 -0400

    Updates.

--- src/components/Jumbotron/index.tsx
+++ src/components/Jumbotron/index.tsx
@@ -1,61 +1,140 @@
-import { LuShip, LuBadgeCheck, LuHeartHandshake, LuBrain } from "react-icons/lu";
+import {
+  LuShip,
+  LuBadgeCheck,
+  LuHeartHandshake,
+  LuBrain,
+} from "react-icons/lu";
 import Flywheel from "../../styles/assets/flywheel-logo-white.svg";
 import Perpetua from "../../styles/assets/perpetua-logo-white.svg";
 import LostAndFound from "../../styles/assets/lostandfound-logo-white.svg";
 import Shoplogix from "../../styles/assets/shoplogix-logo-white.svg";
+import Stan from "../../styles/assets/stan-logo-white.svg";
+import StackAdapt from "../../styles/assets/stackadapt-logo-white.svg";
+
+import FlywheelIcon from "../../styles/assets/flywheel-icon-white.svg";
+import PerpetuaIcon from "../../styles/assets/perpetua-icon-white.svg";
+import LostAndFoundIcon from "../../styles/assets/lostandfound-icon-white.svg";
+import ShoplogixIcon from "../../styles/assets/shoplogix-icon-white.svg";
+import StanIcon from "../../styles/assets/stan-icon-white.svg";
+import StackAdaptIcon from "../../styles/assets/stackadapt-icon-white.svg";
 
 const Jumbotron = () => {
-    return (
-        <div className={"relative w-full h-[600px] sm:h-screen flex flex-col gap-4 items-center justify-center px-6 sm:flex-row sm:gap-2 sm:px-28 radi"}>
-            <div className={"w-full flex flex-col gap-1"}>
-                <div className={"-rotate-6 rounded-md w-fit py-0.5 px-2 text-sm font-semibold text-white bg-slate-800"}>
-                    Trusted by devs, businesses, & startups
-                </div>
-                
-                <p className={"text-white text-xl-3 sm:text-xxl font-black"}>
-                    Experienced Engineer leveraging code and design
-                </p>
-            </div>
-            <div className={"w-full flex flex-col gap-8 sm:w-1/2"}>
-                <div className={"flex gap-3 text-white items-center"}>
-                    <LuShip size={32} />
-                    <p className={"font-semibold"}>
-                        Shipped many high priority features
-                    </p>
-                </div>
-
-                <div className={"flex gap-3 text-white items-center"}>
-                    <LuBadgeCheck size={32} />
-                    <p className={"font-semibold"}>
-                        Reviewed hundreds of PRs
-                    </p>
-                </div>
-
-                <div className={"flex gap-3 text-white items-center"}>
-                    <LuBrain size={32} />
-                    <p className={"font-semibold"}>
-                        Built products with cross-functional teams
-                    </p>
-                </div>
-
-                <div className={"flex gap-3 text-white items-center"}>
-                    <LuHeartHandshake size={32} />
-                    <p className={"font-semibold"}>
-                        Collaborated with local businesses
-                    </p>
-                </div>
-            </div>
-
-            <div className={"absolute bottom-0 w-full flex items-center justify-center py-6"}>
-                <div className={"w-full px-2 lg:px-0 lg:w-2/3 flex justify-between items-center"}>
-                    <img src={Perpetua} className={"h-4 md:h-8"} alt={"Perpetua White Logo"} />
-                    <img src={Flywheel} className={"h-4 md:h-8"} alt={"Flywheel White Logo"} />
-                    <img src={LostAndFound} className={"h-4 md:h-7"} alt={"Lost & Found White Logo"} />
-                    <img src={Shoplogix} className={"h-4 md:h-8"} alt={"Shoplogix White Logo"} />
-                </div>
-            </div>
+  return (
+    <div
+      className={
+        "relative w-full h-[600px] sm:h-screen flex flex-col gap-4 items-center justify-center px-6 sm:flex-row sm:gap-2 sm:px-28 radi"
+      }
+    >
+      <div className={"w-full flex flex-col gap-1"}>
+        <div
+          className={
+            "-rotate-6 rounded-md w-fit py-0.5 px-2 text-sm font-semibold text-white bg-slate-800"
+          }
+        >
+          Trusted by devs, businesses, & startups
         </div>
-    )
-}
+
+        <p className={"text-white text-xl-3 sm:text-xxl font-black"}>
+          Experienced Engineer leveraging code and design
+        </p>
+      </div>
+      <div className={"w-full flex flex-col gap-8 sm:w-1/2"}>
+        <div className={"flex gap-3 text-white items-center"}>
+          <LuShip size={32} />
+          <p className={"font-semibold"}>Shipped many high priority features</p>
+        </div>
+
+        <div className={"flex gap-3 text-white items-center"}>
+          <LuBadgeCheck size={32} />
+          <p className={"font-semibold"}>Reviewed hundreds of PRs</p>
+        </div>
+
+        <div className={"flex gap-3 text-white items-center"}>
+          <LuBrain size={32} />
+          <p className={"font-semibold"}>
+            Built products with cross-functional teams
+          </p>
+        </div>
+
+        <div className={"flex gap-3 text-white items-center"}>
+          <LuHeartHandshake size={32} />
+          <p className={"font-semibold"}>Collaborated with local businesses</p>
+        </div>
+      </div>
+
+      <div
+        className={
+          "absolute bottom-0 w-full flex items-center justify-center py-6"
+        }
+      >
+        <div className={"w-full px-6 flex justify-between items-center"}>
+          {/* Icons */}
+          <img
+            src={StackAdaptIcon}
+            className={"flex h-7 lg:hidden"}
+            alt={"StackAdapt White Icon"}
+          />
+          <img
+            src={PerpetuaIcon}
+            className={"flex h-7 lg:hidden"}
+            alt={"Perpetua White Icon"}
+          />
+          <img
+            src={FlywheelIcon}
+            className={"flex h-7 lg:hidden"}
+            alt={"Flywheel White Icon"}
+          />
+          <img
+            src={StanIcon}
+            className={"flex h-7 lg:hidden"}
+            alt={"Stan White Icon"}
+          />
+          <img
+            src={LostAndFoundIcon}
+            className={"flex h-7 lg:hidden"}
+            alt={"Lost & Found White Icon"}
+          />
+          <img
+            src={ShoplogixIcon}
+            className={"flex h-7 lg:hidden"}
+            alt={"Shoplogix White Icon"}
+          />
+
+          {/* Full Logos */}
+          <img
+            src={StackAdapt}
+            className={"hidden lg:flex md:h-7"}
+            alt={"StackAdapt White Logo"}
+          />
+          <img
+            src={Perpetua}
+            className={"hidden lg:flex md:h-7"}
+            alt={"Perpetua White Logo"}
+          />
+          <img
+            src={Flywheel}
+            className={"hidden lg:flex md:h-7"}
+            alt={"Flywheel White Logo"}
+          />
+          <img
+            src={Stan}
+            className={"hidden lg:flex md:h-7"}
+            alt={"Stan White Logo"}
+          />
+          <img
+            src={LostAndFound}
+            className={"hidden lg:flex md:h-7"}
+            alt={"Lost & Found White Logo"}
+          />
+          <img
+            src={Shoplogix}
+            className={"hidden lg:flex md:h-7"}
+            alt={"Shoplogix White Logo"}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Jumbotron;
--- src/components/PriceSection/index.tsx
+++ src/components/PriceSection/index.tsx
@@ -1,105 +1,236 @@
 import { useState } from "react";
-import { LuCode2, LuSparkles, LuPackage, LuBriefcase } from "react-icons/lu";
+import {
+  LuCode2,
+  LuSparkles,
+  LuPackage,
+  LuBriefcase,
+  LuPaintbrush,
+} from "react-icons/lu";
 import PDF from "../../files/aliSanaknaki_2024.pdf";
 
 const PriceSection = () => {
-    const [isHoveringLost, setIsHoveringLost] = useState<boolean>(false);
-    const [isHoveringSurf, setIsHoveringSurf] = useState<boolean>(false);
+  const [isHoveringLost, setIsHoveringLost] = useState<boolean>(false);
+  const [isHoveringSurf, setIsHoveringSurf] = useState<boolean>(false);
 
-    return (
-        <div className={"text-slate-800 w-full flex flex-col gap-4 items-center justify-center p-6 sm:flex-row sm:gap-28 sm:p-28"}>
-
-            <div className={"p-6 rounded-md flex bg-white w-full flex-col gap-6 shadow max-w-96"}>
-                <div className={"w-full flex justify-between"}>
-                    <p className={"text-xl-2 font-bold"}>Enterprise</p>
-                    <p className={"text-xl-2 font-bold text-gray-500"}>$0<span className={"text-lg font-medium"}>/forever</span></p>
-                </div>
-
-                <div className={"flex flex-col gap-3"}>
-                    <p className={"text-lg font-bold"}>Professional</p>
-
-                    <div className={"flex w-full gap-4 items-center"}>
-                        <div className={"p-2 text-white aspect-square rounded-full bg-slate-800"}>
-                            <LuBriefcase size={16} />
-                        </div>
-
-                        <p className={"text-base"}><span className={"font-semibold"}>Flywheel / Perpetua (Present)</span> — Senior Software Engineer</p>
-                    </div>
-
-                    <div className={"flex w-full gap-4 items-center"}>
-
-                        <div className={"p-2 text-white aspect-square rounded-full bg-slate-800"}>
-                            <LuBriefcase size={16} />
-                        </div>
-
-                        <p className={"text-base"}><span className={"font-semibold"}>Shoplogix (02/19 - 04/21)</span> — Frontend Engineer</p>
-                    </div>
-                </div>
-
-                <div className={"flex flex-col gap-3"}>
-                    <p className={"text-lg font-bold"}>Projects</p>
-
-                    <div className={"flex w-full gap-4 items-center cursor-pointer"} onMouseEnter={() => setIsHoveringLost(true)} onMouseLeave={() => setIsHoveringLost(false)} onClick={() => window.open("https://www.shoplostfound.com", "_blank")}>
-                        <div className={`p-2 text-white aspect-square rounded-full ${isHoveringLost ? "radiNoWhite" : "bg-slate-800"}`}>
-                            <LuSparkles size={16} />
-                        </div>
-
-                        <p className={"text-base"}><span className={"font-semibold"}>Lost & Found</span> — Website re-design</p>
-                    </div>
-
-                    <div className={"flex w-full gap-4 items-center cursor-pointer"} onMouseEnter={() => setIsHoveringSurf(true)} onMouseLeave={() => setIsHoveringSurf(false)} onClick={() => window.open("https://www.uottawa.ca/tabaret/en/content/interactive-student-art-adds-creative-spark-stem", "_blank")}>
-                        <div className={`p-2 text-white aspect-square rounded-full bg-slate-800 ${isHoveringSurf ? "radiNoWhite" : "bg-slate-800"}`}>
-                            <LuSparkles size={16} />
-                        </div>
-
-                        <p className={"text-base"}><span className={"font-semibold"}>Surface Tension</span> — Engineering design installation</p>
-                    </div>
-                </div>
-
-                <div className={"flex flex-col gap-3"}>
-                    <p className={"text-lg font-bold"}>Skills</p>
-
-                    <div className={"flex w-full gap-4 items-center"}>
-                        <div className={"p-2 text-white aspect-square rounded-full bg-slate-800"}>
-                            <LuCode2 size={16} />
-                        </div>
-
-                        <p className={"text-base"}><span className={"font-semibold"}>Development</span> — React, Typescript, Javascript, GraphQL, TailwindCSS</p>
-                    </div>
-
-                    <div className={"flex w-full gap-4 items-center"}>
-
-                        <div className={"p-2 text-white aspect-square rounded-full bg-slate-800"}>
-                            <LuPackage size={16} />
-                        </div>
-
-                        <p className={"text-base"}><span className={"font-semibold"}>Services</span> — Firebase, Git, Jira, Postman, Launch Darkly, Contentful, Miro</p>
-                    </div>
-                </div>
-
-
-                <div className={"w-full flex gap-2"}>
-                    <div className={"w-full rounded-md p-2.5 flex justify-center text-slate-800"}>
-                        <p className={"text-lg font-medium cursor-pointer text-gray-500"} onClick={() => window.open(PDF, "_blank")}>View resume</p>
-                    </div>
-
-                    <div className={"w-full rounded-md p-2.5 flex justify-center text-white cursor-pointer radiNoWhite"}
-                    onClick={() => {
-                        const email = "sanaknaki.ali@gmail.com";
-                        const subject = "We're Interested!";
-                        const body = "Ali,";
-        
-                        const mailtoLink = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
-        
-                        // Open the mailto link in a new tab or window
-                        window.location.href = mailtoLink;
-                    }}>
-                        <p className={"text-lg font-medium"}>Get in contact</p>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div
+      className={
+        "text-slate-800 w-full flex flex-col gap-4 items-center justify-center p-6 sm:flex-row sm:gap-28 sm:p-28"
+      }
+    >
+      <div
+        className={
+          "p-6 rounded-md flex bg-white w-full flex-col gap-6 shadow max-w-96"
+        }
+      >
+        <div className={"w-full flex justify-between"}>
+          <p className={"text-xl-2 font-bold"}>Enterprise</p>
+          <p className={"text-xl-2 font-bold text-gray-500"}>
+            $0<span className={"text-lg font-medium"}>/forever</span>
+          </p>
         </div>
-    )
-}
+
+        <div className={"flex flex-col gap-3"}>
+          <p className={"text-lg font-bold"}>Professional</p>
+
+          <div className={"flex w-full gap-4 items-center"}>
+            <div
+              className={
+                "p-2 text-white aspect-square rounded-full bg-slate-800"
+              }
+            >
+              <LuBriefcase size={16} />
+            </div>
+
+            <p className={"text-base"}>
+              <span className={"font-semibold"}>StackAdapt (Present)</span> — UI
+              / UX Software Engineer
+            </p>
+          </div>
+
+          <div className={"flex w-full gap-4 items-center"}>
+            <div
+              className={
+                "p-2 text-white aspect-square rounded-full bg-slate-800"
+              }
+            >
+              <LuBriefcase size={16} />
+            </div>
+
+            <p className={"text-base"}>
+              <span className={"font-semibold"}>Stan</span> — Founding Senior
+              Frontend Software Engineer
+            </p>
+          </div>
+
+          <div className={"flex w-full gap-4 items-center"}>
+            <div
+              className={
+                "p-2 text-white aspect-square rounded-full bg-slate-800"
+              }
+            >
+              <LuBriefcase size={16} />
+            </div>
+
+            <p className={"text-base"}>
+              <span className={"font-semibold"}>Perpetua / Flywheel</span> —
+              Senior Software Engineer
+            </p>
+          </div>
+
+          <div className={"flex w-full gap-4 items-center"}>
+            <div
+              className={
+                "p-2 text-white aspect-square rounded-full bg-slate-800"
+              }
+            >
+              <LuBriefcase size={16} />
+            </div>
+
+            <p className={"text-base"}>
+              <span className={"font-semibold"}>Shoplogix</span> — Frontend
+              Engineer
+            </p>
+          </div>
+        </div>
+
+        <div className={"flex flex-col gap-3"}>
+          <p className={"text-lg font-bold"}>Projects</p>
+
+          <div
+            className={"flex w-full gap-4 items-center cursor-pointer"}
+            onMouseEnter={() => setIsHoveringLost(true)}
+            onMouseLeave={() => setIsHoveringLost(false)}
+            onClick={() =>
+              window.open("https://www.shoplostfound.com", "_blank")
+            }
+          >
+            <div
+              className={`p-2 text-white aspect-square rounded-full ${
+                isHoveringLost ? "radiNoWhite" : "bg-slate-800"
+              }`}
+            >
+              <LuSparkles size={16} />
+            </div>
+
+            <p className={"text-base"}>
+              <span className={"font-semibold"}>Lost & Found</span> — Website
+              re-design
+            </p>
+          </div>
+
+          <div
+            className={"flex w-full gap-4 items-center cursor-pointer"}
+            onMouseEnter={() => setIsHoveringSurf(true)}
+            onMouseLeave={() => setIsHoveringSurf(false)}
+            onClick={() =>
+              window.open(
+                "https://www.uottawa.ca/tabaret/en/content/interactive-student-art-adds-creative-spark-stem",
+                "_blank"
+              )
+            }
+          >
+            <div
+              className={`p-2 text-white aspect-square rounded-full bg-slate-800 ${
+                isHoveringSurf ? "radiNoWhite" : "bg-slate-800"
+              }`}
+            >
+              <LuSparkles size={16} />
+            </div>
+
+            <p className={"text-base"}>
+              <span className={"font-semibold"}>Surface Tension</span> —
+              Engineering design installation
+            </p>
+          </div>
+        </div>
+
+        <div className={"flex flex-col gap-3"}>
+          <p className={"text-lg font-bold"}>Skills</p>
+
+          <div className={"flex w-full gap-4 items-center"}>
+            <div
+              className={
+                "p-2 text-white aspect-square rounded-full bg-slate-800"
+              }
+            >
+              <LuCode2 size={16} />
+            </div>
+
+            <p className={"text-base"}>
+              <span className={"font-semibold"}>Development</span> — React,
+              Typescript, Javascript, GraphQL, TailwindCSS
+            </p>
+          </div>
+
+          <div className={"flex w-full gap-4 items-center"}>
+            <div
+              className={
+                "p-2 text-white aspect-square rounded-full bg-slate-800"
+              }
+            >
+              <LuPackage size={16} />
+            </div>
+
+            <p className={"text-base"}>
+              <span className={"font-semibold"}>Services</span> — Firebase, Git,
+              Jira, Postman, Launch Darkly, Contentful, Miro
+            </p>
+          </div>
+
+          <div className={"flex w-full gap-4 items-center"}>
+            <div
+              className={
+                "p-2 text-white aspect-square rounded-full bg-slate-800"
+              }
+            >
+              <LuPaintbrush size={16} />
+            </div>
+
+            <p className={"text-base"}>
+              <span className={"font-semibold"}>Design</span> — Figma, Adobe
+              Illustrator
+            </p>
+          </div>
+        </div>
+
+        <div className={"w-full flex gap-2"}>
+          <div
+            className={
+              "w-full rounded-md p-2.5 flex justify-center text-slate-800"
+            }
+          >
+            <p
+              className={"text-lg font-medium cursor-pointer text-gray-500"}
+              onClick={() => window.open(PDF, "_blank")}
+            >
+              View resume
+            </p>
+          </div>
+
+          <div
+            className={
+              "w-full rounded-md p-2.5 flex justify-center text-white cursor-pointer radiNoWhite"
+            }
+            onClick={() => {
+              const email = "sanaknaki.ali@gmail.com";
+              const subject = "We're Interested!";
+              const body = "Ali,";
+
+              const mailtoLink = `mailto:${email}?subject=${encodeURIComponent(
+                subject
+              )}&body=${encodeURIComponent(body)}`;
+
+              // Open the mailto link in a new tab or window
+              window.location.href = mailtoLink;
+            }}
+          >
+            <p className={"text-lg font-medium"}>Get in contact</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default PriceSection;
