b9a23cbfa35fe54621fea8ddd06a9351877980b0
Ali Sanaknaki <sanaknaki.ali@gmail.com>
2025-08-24 15:42:36 -0400

    Updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,19 +21,35 @@ const Home = () => {
 
   return (
     <div className="bg-white h-screen w-screen flex items-center justify-center cursor-default">
-      {/* Left Panel / Top Panel*/}
-      <div className="absolute top-2 md:top-0 left-0 md:left-2 w-full md:w-fit h-fit md:h-full flex gap-5 flex-row md:flex-col items-center justify-center">
+      {!hoveringIcon ? (
+        <p className="text-2xl md:text-4xl font-mnews">
+          Ali Sanaknaki,{" "}
+          <span className="bg-[#1E42F6] text-white">Design Engine</span>er.
+        </p>
+      ) : (
+        <p
+          className="text-2xl md:text-4xl font-mnews"
+          dangerouslySetInnerHTML={{ __html: hoveringIcon }}
+        />
+      )}
+      {/* Right Panel / Bottom Panel*/}
+      <div className="absolute bottom-4 md:bottom-0 right-0 md:right-4 w-full md:w-fit h-fit md:h-full flex gap-5 flex-row md:flex-col items-center justify-center">
         <Image
           src="/everydayworks.svg"
           alt="Everyday Works"
           width={20}
           height={20}
-          className="cursor-pointer"
-          onMouseEnter={() =>
+          className={`cursor-pointer ${
+            lastTappedIcon !== "" && lastTappedIcon !== "everydayworks"
+              ? "blur-sm transition-all duration-100 ease-in-out"
+              : ""
+          }`}
+          onMouseEnter={() => {
             setHoveringIcon(
               "<span style='background-color: #1E42F6; color: white; padding: 0 4px;'>Everyday Works</span>, Design Portfolio."
-            )
-          }
+            );
+            setLastTappedIcon("everydayworks");
+          }}
           onClick={() => {
             if (isMobile) {
               if (lastTappedIcon === "everydayworks") {
@@ -52,33 +68,27 @@ const Home = () => {
               window.open("https://everyday-works.com/", "_blank");
             }
           }}
-          onMouseLeave={() => setHoveringIcon("")}
+          onMouseLeave={() => {
+            setHoveringIcon("");
+            setLastTappedIcon("");
+          }}
         />
-      </div>
-      {!hoveringIcon ? (
-        <p className="text-2xl md:text-4xl font-mnews">
-          Ali Sanaknaki,{" "}
-          <span className="bg-[#1E42F6] text-white">Design Engine</span>er.
-        </p>
-      ) : (
-        <p
-          className="text-2xl md:text-4xl font-mnews"
-          dangerouslySetInnerHTML={{ __html: hoveringIcon }}
-        />
-      )}
-      {/* Right Panel / Bottom Panel*/}
-      <div className="absolute bottom-2 md:bottom-0 right-0 md:right-2 w-full md:w-fit h-fit md:h-full flex gap-5 flex-row md:flex-col items-center justify-center">
         <Image
           src="/linkedin.svg"
           alt="LinkedIn"
           width={20}
           height={20}
-          className="cursor-pointer"
-          onMouseEnter={() =>
+          className={`cursor-pointer ${
+            lastTappedIcon !== "" && lastTappedIcon !== "linkedin"
+              ? "blur-sm transition-all duration-100 ease-in-out"
+              : ""
+          }`}
+          onMouseEnter={() => {
             setHoveringIcon(
               "Let's <span style='background-color: #1E42F6; color: white; padding: 0 4px;'>Link</span>edIn."
-            )
-          }
+            );
+            setLastTappedIcon("linkedin");
+          }}
           onClick={() => {
             if (isMobile) {
               if (lastTappedIcon === "linkedin") {
@@ -103,19 +113,27 @@ const Home = () => {
               );
             }
           }}
-          onMouseLeave={() => setHoveringIcon("")}
+          onMouseLeave={() => {
+            setHoveringIcon("");
+            setLastTappedIcon("");
+          }}
         />
         <Image
           src="/github.svg"
           alt="GitHub"
           width={20}
           height={20}
-          className="cursor-pointer"
-          onMouseEnter={() =>
+          className={`cursor-pointer ${
+            lastTappedIcon !== "" && lastTappedIcon !== "github"
+              ? "blur-sm transition-all duration-100 ease-in-out"
+              : ""
+          }`}
+          onMouseEnter={() => {
             setHoveringIcon(
-              "Pushing <span style='background-color: #1E42F6; color: white; padding: 0 4px;'>P</span>Rs."
-            )
-          }
+              "<span style='background-color: #1E42F6; color: white; padding: 0 4px;'>Pushing P</span>Rs."
+            );
+            setLastTappedIcon("github");
+          }}
           onClick={() => {
             if (isMobile) {
               if (lastTappedIcon === "github") {
@@ -134,19 +152,27 @@ const Home = () => {
               window.open("https://github.com/sanaknaki", "_blank");
             }
           }}
-          onMouseLeave={() => setHoveringIcon("")}
+          onMouseLeave={() => {
+            setHoveringIcon("");
+            setLastTappedIcon("");
+          }}
         />
         <Image
           src="/inbox.svg"
           alt="Inbox"
           width={20}
           height={20}
-          className="cursor-pointer"
-          onMouseEnter={() =>
+          className={`cursor-pointer ${
+            lastTappedIcon !== "" && lastTappedIcon !== "inbox"
+              ? "blur-sm transition-all duration-100 ease-in-out"
+              : ""
+          }`}
+          onMouseEnter={() => {
             setHoveringIcon(
               "Hit my <span style='background-color: #1E42F6; color: white; padding: 0 4px;'>line.</span>"
-            )
-          }
+            );
+            setLastTappedIcon("inbox");
+          }}
           onClick={() => {
             if (isMobile) {
               if (lastTappedIcon === "inbox") {
@@ -169,7 +195,10 @@ const Home = () => {
               link.click();
             }
           }}
-          onMouseLeave={() => setHoveringIcon("")}
+          onMouseLeave={() => {
+            setHoveringIcon("");
+            setLastTappedIcon("");
+          }}
         />
       </div>
     </div>
